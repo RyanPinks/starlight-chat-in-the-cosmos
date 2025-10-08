5756a61906d192976b75a045624cb6f8e469b961
RyanPinks <ryanpinks64@gmail.com>
2025-10-08 12:37:11 -0700

    Update aiva_memory.py

--- aiva/aiva_memory.py
+++ aiva/aiva_memory.py
@@ -1,1 +1,11 @@
+# aiva_memory.py
 
+class AivaMemory:
+    def __init__(self):
+        self.log = []
+
+    def record_message(self, speaker, message):
+        self.log.append((speaker, message))
+
+    def get_last(self):
+        return self.log[-1] if self.log else None
